e86c24e93d7ac2d6c3c7113f00ea938acef3b00f
jameela-k <jameela.khalil.95@gmail.com>
2026-09-08 19:27:43 +0300

    created the server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+/* =========================================================
+   MISSION I: THE FALLEN GATES OF CODORIA
+
+   The Dark Bug has damaged the Royal Server!
+   Work with your team to locate and repair THREE bugs.
+
+   Do not delete the code and start again.
+   Read the errors, test your ideas, and repair what is broken.
+   ========================================================= */
+
+const express = require('express')
+
+const app = express
+
+app.get('gate', function (req, res) {
+  res.sent('🏰 The gates of Codoria are open!')
+})
+
+app.listen(3000, function () {
+  console.log('⚔️ Royal server is running on port 3000')
+})
+
+/* =========================================================
+   ⭐ BONUS QUEST
+
+   Create a new GET route for /king that displays:
+   👑 Welcome, Your Majesty!
+   ========================================================= */
